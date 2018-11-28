43713140c8863e872cb6f632b8b349eaabf2b1b5
Pieter <pieter_du_toit@hotmail.com>
2018-11-28 19:05:24 +0200

    add react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Link } from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
-      <div> MERN APP </div>
+      <Router>
+        <div>react-express-app</div>
+      </Router>
     );
   }
 }
