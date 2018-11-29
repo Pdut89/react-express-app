050c3cd6fe709fa99c1835225382125cf6d01673
Pieter <pdutoit@fullfacing.com>
2018-11-29 10:05:36 +0200

    add pages/components folders

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Link } from "react-router-dom";
+import React, { Component } from 'react'
+import { BrowserRouter as Router } from 'react-router-dom'
+import Base from './pages/base'
 
 class App extends Component {
   render() {
     return (
       <Router>
-        <div>react-express-app</div>
+        <Base/>
       </Router>
     );
   }
---
+++ src/pages/base.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Base = () => {
+  return (
+    <div>React app</div>
+  )
+}
+
+export default Base
